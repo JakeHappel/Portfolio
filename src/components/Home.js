a504ee01c76f70d176c9a4b6c58f64d6b6585715
JakeHappel <jjhappel4213@gmail.com>
2023-01-09 19:22:34 -0500

    project section

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
         <body>
             <div className="Home">
             <div className="FirstP">
-           <h2>Hello!, I'm Jake a Web Developer</h2>
+           <h2>Hello! I'm Jake a Web Developer</h2>
            <p>I'm fluent in HTML, CSS, and Javascript. I have found that nothing satisfies me more than project collaboration, cultivating new relationships, solving problems, and contributing to business growth through technology.</p>
            </div>
            <Icons />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+var Potluck = require("../images/Potluck.png");
+var NasaPhoto = require("../images/NasaPhoto.png");
 const Projects = () => {
     return(
-        <></>
+        <body>
+        <div className="Projects">
+        <img className="Potluck" src={Potluck} alt='Potluck'/>
+        <img className="Nasa" src={NasaPhoto} alt='NasaPhoto'/>
+        </div>
+        </body>
     )
 }
 export default Projects
